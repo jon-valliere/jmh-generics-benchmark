1ad61821f9cb71c34812c61a97e45413cd1bb25a
Jonathan Valliere <jon.valliere@emoten.com>
2019-11-05 08:51:19 -0500

    misc

---
+++ src/main/java/bench/BakedGenericInterfaceMultipleArgsTest.java
@@ -0,0 +1,153 @@
+package bench;
+
+import org.openjdk.jmh.annotations.Benchmark;
+
+/**
+ * Tests against a Type which has fully baked a Generic Abstract Method
+ */
+public class BakedGenericInterfaceMultipleArgsTest
+{
+	@Benchmark
+	public void testExplicit()
+	{
+		final ReceiverType x = this.createExplicitReceiver();
+
+		final BaseType y = this.createObject();
+
+		BaseType z = null;
+
+		for (int i = 0; i != 1_000_000; i++)
+		{
+			if (z == y)
+			{
+				z = x.foo(y, y);
+			}
+			else
+			{
+				z = x.foo(y, y);
+			}
+		}
+	}
+
+	@Benchmark
+	public void testChecked()
+	{
+		final AbstractInterface<BaseType> x = this.createCheckedReceiver();
+
+		final BaseType y = this.createObject();
+
+		BaseType z = null;
+
+		for (int i = 0; i != 1_000_000; i++)
+		{
+			if (z == y)
+			{
+				z = x.foo(y, y);
+			}
+			else
+			{
+				z = x.foo(y, y);
+			}
+		}
+	}
+
+	@Benchmark
+	public void testUnchecked()
+	{
+		final AbstractInterface x = this.createUncheckedReceiver();
+
+		final Object y = this.createObject();
+
+		Object z = null;
+
+		for (int i = 0; i != 1_000_000; i++)
+		{
+			if (z == y)
+			{
+				z = x.foo(y, y);
+			}
+			else
+			{
+				z = x.foo(y, y);
+			}
+		}
+	}
+
+	@Benchmark
+	public void testBaked()
+	{
+		final BakedInterface x = this.createBakedReceiver();
+
+		final BaseType y = this.createObject();
+
+		BaseType z = null;
+
+		for (int i = 0; i != 1_000_000; i++)
+		{
+			if (z == y)
+			{
+				z = x.foo(y, y);
+			}
+			else
+			{
+				z = x.foo(y, y);
+			}
+		}
+	}
+
+	protected BaseType createObject()
+	{
+		return new BaseType();
+	}
+
+	protected ReceiverType createExplicitReceiver()
+	{
+		return new ReceiverType();
+	}
+
+	protected AbstractInterface<BaseType> createCheckedReceiver()
+	{
+		return new ReceiverType();
+	}
+
+	protected AbstractInterface createUncheckedReceiver()
+	{
+		return new ReceiverType();
+	}
+
+	protected BakedInterface createBakedReceiver()
+	{
+		return new ReceiverType();
+	}
+
+	static private class ReceiverType extends BaseType implements BakedInterface
+	{
+		@Override
+		public BaseType foo(final BaseType value, final BaseType bar)
+		{
+			if (value.id == bar.id)
+			{
+				return value;
+			}
+			else
+			{
+				return value;
+			}
+		}
+	}
+
+	static private interface BakedInterface extends AbstractInterface<BaseType>
+	{
+
+	}
+
+	static private interface AbstractInterface<X>
+	{
+		X foo(X value, X bar);
+	}
+
+	static private class BaseType
+	{
+		public int id = 0;
+	}
+}
---
+++ src/main/java/bench/BakedGenericTypeTest.java
@@ -0,0 +1,160 @@
+package bench;
+
+import org.openjdk.jmh.annotations.Benchmark;
+
+/**
+ * Tests against a Type which has fully baked a Generic Abstract Method
+ */
+public class BakedGenericTypeTest
+{
+	@Benchmark
+	public void testExplicit()
+	{
+		final ReceiverType x = this.createExplicitReceiver();
+
+		final BaseType y = this.createObject();
+
+		BaseType z = null;
+
+		for (int i = 0; i != 1_000_000; i++)
+		{
+			if (z == y)
+			{
+				z = x.foo(y);
+			}
+			else
+			{
+				z = x.foo(y);
+			}
+		}
+	}
+
+	@Benchmark
+	public void testChecked()
+	{
+		final GenericType<BaseType> x = this.createCheckedReceiver();
+
+		final BaseType y = this.createObject();
+
+		BaseType z = null;
+
+		for (int i = 0; i != 1_000_000; i++)
+		{
+			if (z == y)
+			{
+				z = x.foo(y);
+			}
+			else
+			{
+				z = x.foo(y);
+			}
+		}
+	}
+
+	@Benchmark
+	public void testUnchecked()
+	{
+		final GenericType x = this.createUncheckedReceiver();
+
+		final Object y = this.createObject();
+
+		Object z = null;
+
+		for (int i = 0; i != 1_000_000; i++)
+		{
+			if (z == y)
+			{
+				z = x.foo(y);
+			}
+			else
+			{
+				z = x.foo(y);
+			}
+		}
+	}
+
+	@Benchmark
+	public void testBaked()
+	{
+		final BakedType x = this.createBakedReceiver();
+
+		final BaseType y = this.createObject();
+
+		BaseType z = null;
+
+		for (int i = 0; i != 1_000_000; i++)
+		{
+			if (z == y)
+			{
+				z = x.foo(y);
+			}
+			else
+			{
+				z = x.foo(y);
+			}
+		}
+	}
+
+	protected BaseType createObject()
+	{
+		return new BaseType();
+	}
+
+	protected ReceiverType createExplicitReceiver()
+	{
+		return new ReceiverType();
+	}
+
+	protected GenericType<BaseType> createCheckedReceiver()
+	{
+		return new ReceiverType();
+	}
+
+	protected GenericType createUncheckedReceiver()
+	{
+		return new ReceiverType();
+	}
+
+	protected BakedType createBakedReceiver()
+	{
+		return new ReceiverType();
+	}
+
+	static private class ReceiverType extends BakedType
+	{
+		@Override
+		public BaseType foo(final BaseType value)
+		{
+			if (value.id == this.id)
+			{
+				return value;
+			}
+			else
+			{
+				return value;
+			}
+		}
+	}
+
+	/**
+	 * By adding a second abstract class in the inheritance, the
+	 * performance drops by an order of magnitude.
+	 */
+	static private class BakedType extends GenericType<BaseType>
+	{
+
+	}
+
+	static private class GenericType<X> extends BaseType
+	{
+		public X foo(final X value)
+		{
+			return value;
+		}
+	}
+
+	static private class BaseType
+	{
+		public int id = 0;
+	}
+}
